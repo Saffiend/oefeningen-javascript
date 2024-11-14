a3dcc853b1a5cc81a0ad1f6be7cda81f0f489732
Saffiend <302371314@student.rocmondriaan.nl>
2024-11-14 15:28:52 +0100

    oefening 14 afgemaakt

--- oefening 14/Oefening14.js
+++ oefening 14/Oefening14.js
@@ -3,18 +3,17 @@ function printHelloWorld() {
 }
 printHelloWorld();
 
-let num = 7
+
 function doubleNumber(num) {
   return num * 2;
 }
-console.log(doubleNumber(num));
+console.log(doubleNumber(7));
 
-let a = 5
-let b = 5
+
 function sumTwoNumbers(a, b) {
   return a + b;
 }
-console.log(sumTwoNumbers(a, b))
+console.log(sumTwoNumbers(5, 5))
 
 
 function toUpperCase(str) {
@@ -27,24 +26,34 @@ toUpperCase(str)
 function isEven(num) {
   return num % 2 === 0;
 }
-console.log(isEven(num))
+console.log(isEven(7))
 
 function sumArray(arr) {
   return arr.reduce((acc, val) => acc + val, 0);
 }
+let arr = [1,2,3,4,5];
+console.log(sumArray(arr));
 
 function getCurrentDateTime() {
   return new Date();
 }
+console.log(getCurrentDateTime());
 
 function calculateAverage(x, y, z) {
   return (x + y + z) / 3;
 }
+console.log(calculateAverage(10, 20, 30));
+
 
 function getFirstAndLast(arr) {
   return [arr[0], arr[arr.length - 1]];
 }
 
+const firstAndLast = getFirstAndLast(['a', 'b', 'c']);
+console.log(firstAndLast[0]);
+console.log(firstAndLast[1]);
+
 function greet(name, greeting) {
   return `${greeting}, ${name}!`;
 }
+console.log(greet('de Lange', 'Hoi'))
